b1657f20c0f4048f96e7ae6363beceeb753de88f
Larry Zhuang <larry.zhuang@ibm.com>
2020-07-15 11:53:29 -0500

    test commit
    - add new func in main
    - updated test

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println(Greet())
+	fmt.Printf(Greet_again())
 }
 
 func Greet() string {
 	return "Hello, World!"
+}
+
+func Greet_again() string {
+     return "Greetings, again!"
 }
--- main_test.go
+++ main_test.go
@@ -8,6 +8,8 @@ import (
 
 func TestGreet(t *testing.T) {
 	if main.Greet() != "Hello, World!" {
-		t.Fail()
+		t.Fail("failed first greeting test")
 	}
+	if main.Greet_again() != "Greetings, again!"
+	   t.fail("failed greeting again test!!")
 }
